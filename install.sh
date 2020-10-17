5171cd638569fc1a699753360b66875f03a2397c
Victor Basso <vitobasso@gmail.com>
2020-10-17 14:48:30 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+TARGET=/usr/local/bin
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo Creating links under $TARGET
+ln -s $DIR/scripts/kubectl-context.pl $TARGET/kctx
+ln -s $DIR/scripts/kubectl-describe.pl $TARGET/kdesc
+ln -s $DIR/scripts/kubectl-exec.pl $TARGET/kexe
+ln -s $DIR/scripts/kubectl-find.pl $TARGET/kfind
+ln -s $DIR/scripts/kubectl-get.pl $TARGET/kget
+ln -s $DIR/scripts/kubectl-init.pl $TARGET/knit #kinit clashes with a Kerberos bin
+ln -s $DIR/scripts/kubectl-logs.pl $TARGET/klog
