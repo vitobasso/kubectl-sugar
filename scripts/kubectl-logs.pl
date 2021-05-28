f082b26c7b206866d361dc1c4d00ca0892e09d48
Victor Basso <vitobasso@gmail.com>
2021-05-28 22:50:31 +0100

    klog: -f to follow

--- scripts/kubectl-logs.pl
+++ scripts/kubectl-logs.pl
@@ -15,12 +15,13 @@ sub help {
 help and exit unless @ARGV;
 
 my $container;
-GetOptions('container|c=s' => \$container) or help and exit;
+my $follow;
+GetOptions('container|c=s' => \$container, 'follow|f' => \$follow) or help and exit;
 
 my $should_retry = "once";
-find_and_exec();
+find_and_log();
 
-sub find_and_exec {
+sub find_and_log {
    my @result = `$RealBin/kubectl-find.pl pod @ARGV`;
    if(scalar @result == 1) {
       my ($ns, $res, $pod) = split " ", shift @result;
@@ -28,9 +29,14 @@ sub find_and_exec {
       if($container) {
         $command = $command . " -c $container";
       }
+      if($follow) {
+        $command = $command . " -f";
+      }
       say $command;
       say "";
-      say `$command`;
+      my $session = Expect->spawn($command);
+      $session->slave->clone_winsize_from(\*STDIN);
+      $session->interact();
       retry($ns) if $?==256 and $should_retry;      
    } elsif (not @result) {
       say "Can't find any pods matching: @ARGV";
@@ -43,5 +49,5 @@ sub retry {
    undef $should_retry;
    my $ns = shift;
    print `$RealBin/kubectl-get.pl -q pod $ns`;
-   find_and_exec();
+   find_and_log();
 }
